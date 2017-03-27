28b5eb6a9fb79209cddf6f6800aa1b240d6241d2
Denis Vasilyev <dinowar1@gmail.com>
2017-03-27 10:03:39 +0300

    WagonType class has been refactored. Deleted not null annotations.

--- datamodule/src/main/java/ru/vasilyev/domain/WagonType.java
+++ datamodule/src/main/java/ru/vasilyev/domain/WagonType.java
@@ -4,20 +4,13 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.NotNull;
-
 @Getter
 @Setter
 @EqualsAndHashCode(callSuper = false)
 public class WagonType extends BaseObject {
 
-    @NotNull
     private String classCode;
-
-    @NotNull
     private String description;
-
-    @NotNull
     private double classCoefficient;
 
 }
