9075dd7c48d3fbf696ad24dbdb0d00f80c787308
Denis Vasilyev <Denis.Vasilyev@t-systems.ru>
2017-03-28 17:29:59 +0300

    Generator class was refactored

--- datamodule/src/main/java/ru/vasilyev/util/Generator.java
+++ datamodule/src/main/java/ru/vasilyev/util/Generator.java
@@ -1,20 +1,27 @@
 package ru.vasilyev.util;
 
 
+import java.util.List;
 import javax.ejb.EJB;
-import javax.ejb.Stateless;
+
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
 import javax.ejb.TransactionManagement;
 import javax.ejb.TransactionManagementType;
+import ru.vasilyev.dao.SeatDao;
 import ru.vasilyev.dao.StationDao;
 import ru.vasilyev.dao.TrainDao;
+import ru.vasilyev.dao.WagonDao;
 import ru.vasilyev.dao.WagonTypeDao;
 import ru.vasilyev.model.Train;
+import ru.vasilyev.model.WagonType;
 
-@Stateless
+@Singleton
+@Startup
 @TransactionManagement(TransactionManagementType.CONTAINER)
-public class TrainGenerator {
+public class Generator {
 
     @EJB
     private WagonTypeDao wagonTypeDao;
@@ -25,11 +32,31 @@ public class TrainGenerator {
     @EJB
     private TrainDao trainDao;
 
+    @EJB
+    private SeatDao seatDao;
+
+    @EJB
+    private WagonDao wagonDao;
+
+
 
     @TransactionAttribute(TransactionAttributeType.REQUIRED)
     public void generateTrain(Train train) {
         trainDao.insertEntity(train);
     }
+
+    public void generateCollectionOfTrains(List<Train> trains) {
+        trainDao.insertCollectionOfEntity(trains);
+    }
+
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    public void generateWagonType(WagonType wagonType) {
+        wagonTypeDao.insertEntity(wagonType);
+    }
+
+
+
+
 
 
 }
